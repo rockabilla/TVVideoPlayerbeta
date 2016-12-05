29076f4ad4b2310a5802e87209c8f616c7011682
rockabilla <rockabilla@yandex.ru>
2016-12-05 15:10:25 +0300

    Start on turn on (not Tested)

---
+++ app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/ActivityRunOnStartup.java
@@ -0,0 +1,20 @@
+package com.piteravto.rockabilla.tvvideoplayerbeta;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by MishustinAI on 05.12.2016.
+ */
+
+public class ActivityRunOnStartup extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
