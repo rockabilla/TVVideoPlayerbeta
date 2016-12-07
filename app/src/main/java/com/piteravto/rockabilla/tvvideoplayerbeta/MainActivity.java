d8e9088c3e8480e2131bdd24a5504e4cd8e6e634
rockabilla <rockabilla@yandex.ru>
2016-12-07 16:58:19 +0300

    No Title (Work)

--- app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/MainActivity.java
+++ app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/MainActivity.java
@@ -3,9 +3,9 @@ package com.piteravto.rockabilla.tvvideoplayerbeta;
 
 import android.content.pm.ActivityInfo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -26,14 +26,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //горизонтальная ориентация
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+
         // установите свой путь к файлу на SD-карточке
-        String videoSource = "/storage/6605-E526/test.mp4";
+        //String videoSource = "/storage/6605-E526/test.mp4";
+        // тут у нас путь, почти не захардкожен (не всегда правильно определяется путь)
+        String videoSource = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/test.mp4";
+
         //Toast.makeText(MainActivity.this, videoSource, Toast.LENGTH_SHORT).show();
+
         videoView = (VideoView) findViewById(R.id.videoview);
 
         videoView.setVideoPath(videoSource);
 
+        //убрали play и прочее
         videoView.setMediaController(null);
         videoView.requestFocus();
         videoView.start(); // начинаем воспроизведение автоматически
