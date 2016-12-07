37d84c8cd19d0d502936d15311a286c8ee349489
rockabilla <rockabilla@yandex.ru>
2016-12-07 19:52:42 +0300

    Download file (not Work)

--- app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/API/ServerApiInterface.java
+++ app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/API/ServerApiInterface.java
@@ -1,8 +1,11 @@
 package com.piteravto.rockabilla.tvvideoplayerbeta.api;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
 
 
 /**
@@ -15,7 +18,9 @@ public interface ServerApiInterface {
 
     //rockabilla пока будем использовать и для получения команды и для посылания уведомления, что он все еще жив
     @GET("{directory}/{command}")
-    Call<String> getData(@Path("directory") String directory, @Path("command") String command);
+    Call<ResponseBody> getData(@Path("directory") String directory, @Path("command") String command);
 
-
+    @Streaming
+    @GET("tv-service/uploads/d9e28907f1a7d949c7d973cdcc2e363c.mp4")
+    Call<ResponseBody> downloadVideo();
 }
--- app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/MainActivity.java
+++ app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/MainActivity.java
@@ -3,6 +3,7 @@ package com.piteravto.rockabilla.tvvideoplayerbeta;
 
 import android.content.pm.ActivityInfo;
 
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,13 @@ import android.widget.VideoView;
 import com.piteravto.rockabilla.tvvideoplayerbeta.api.ServerApi;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,41 +38,162 @@ public class MainActivity extends AppCompatActivity {
         //горизонтальная ориентация
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-
-
+        /*
         // установите свой путь к файлу на SD-карточке
         //String videoSource = "/storage/6605-E526/test.mp4";
         // тут у нас путь, почти не захардкожен (не всегда правильно определяется путь)
         String videoSource = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/test.mp4";
-
         //Toast.makeText(MainActivity.this, videoSource, Toast.LENGTH_SHORT).show();
 
+
         videoView = (VideoView) findViewById(R.id.videoview);
-
         videoView.setVideoPath(videoSource);
-
         //убрали play и прочее
         videoView.setMediaController(null);
         videoView.requestFocus();
         videoView.start(); // начинаем воспроизведение автоматически
+        */
+        //Toast.makeText(MainActivity.this, getString(R.string.server_name) + "tv-service/uploads/d9e28907f1a7d949c7d973cdcc2e363c.mp4", Toast.LENGTH_LONG).show();
+
+        downloadVideo("tv-service/uploads/d9e28907f1a7d949c7d973cdcc2e363c.mp4");
+        //getCommand();
+
+        //http://tabus.piteravto.ru/
     }
 
-
-
-
-
-    //получаем инструкцию что нам делать
-    private void getCommand ()
+    private void downloadVideo1 ()
     {
-        ServerApi.getApi().getData(getString(R.string.tv_directory), getString(R.string.get_command)).enqueue(new Callback<String>() {
+        ServerApi.getApi().downloadVideo().enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                command = response.body();
-                //recyclerView.getAdapter().notifyDataSetChanged();
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                if (response.isSuccessful()) {
+                    //Log.d(TAG, "server contacted and has file");
+                    Toast.makeText(MainActivity.this, "server contacted and has file", Toast.LENGTH_SHORT).show();
+                    boolean writtenToDisk = writeVideoToDisk(response.body());
+
+                    //Log.d(TAG, "file download was a success? " + writtenToDisk);
+                } else {
+                    //Log.d(TAG, "server contact failed");
+                }
             }
 
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                //Log.e(TAG, "error");
+            }
+        });
+    }
+
+    private void downloadVideo (final String url)
+    {
+        new AsyncTask<Void, Long, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                ServerApi.getApi().downloadVideo().enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        Toast.makeText(MainActivity.this, "begin", Toast.LENGTH_SHORT).show();
+                        if (response.isSuccessful()) {
+
+                            //Log.d(TAG, "server contacted and has file");
+                            Toast.makeText(MainActivity.this, "server contacted and has file", Toast.LENGTH_SHORT).show();
+                            boolean writtenToDisk = writeVideoToDisk(response.body());
+
+                            //Log.d(TAG, "file download was a success? " + writtenToDisk);
+                            Toast.makeText(MainActivity.this, "file download was a success? ", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            //Log.d(TAG, "server contact failed");
+                            Toast.makeText(MainActivity.this, "server contact failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        //Log.e(TAG, "error");
+                        Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return null;
+            }
+        }.execute();
+    }
+
+    private boolean writeVideoToDisk(ResponseBody body) {
+        try {
+            // todo change the file location/name according to your needs
+            File futureStudioIconFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + "video.mp4");
+
+            Toast.makeText(MainActivity.this, Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + "video.mp4", Toast.LENGTH_LONG).show();
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(futureStudioIconFile);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+
+                    fileSizeDownloaded += read;
+
+                    //Log.d(TAG, "file download: " + fileSizeDownloaded + " of " + fileSize);
+                    Toast.makeText(MainActivity.this, "file download: " + fileSizeDownloaded + " of " + fileSize, Toast.LENGTH_SHORT).show();
+                }
+
+                outputStream.flush();
+
+                return true;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+
+    //получаем инструкцию что нам делать
+    private void getCommand()
+    {
+        ServerApi.getApi().getData(getString(R.string.tv_directory), getString(R.string.get_command)).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Toast.makeText(MainActivity.this, "Успешно подключился", Toast.LENGTH_LONG).show();
+
+                try {
+                    command = response.body().string();
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, "Ошибка string()", Toast.LENGTH_SHORT).show();
+                }
+
+                //recyclerView.getAdapter().notifyDataSetChanged();
+
+                Toast.makeText(MainActivity.this, command, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "Ошибка подключения", Toast.LENGTH_SHORT).show();
             }
         });
@@ -73,14 +202,14 @@ public class MainActivity extends AppCompatActivity {
     //просто стучимся, чтобы дать знать, что мы еще живы
     private void sendNotification()
     {
-        ServerApi.getApi().getData(getString(R.string.tv_directory), getString(R.string.send_notification)).enqueue(new Callback<String>() {
+        ServerApi.getApi().getData(getString(R.string.tv_directory), getString(R.string.send_notification)).enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<String> call, Response<String> response) {
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
 
             }
 
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "Ошибка подключения", Toast.LENGTH_SHORT).show();
             }
         });
