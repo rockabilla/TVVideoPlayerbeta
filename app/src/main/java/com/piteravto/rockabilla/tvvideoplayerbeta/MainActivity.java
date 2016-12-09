a8cea5b743114cf95ab377802c44d1604937f8dd
rockabilla <rockabilla@yandex.ru>
2016-12-09 14:14:41 +0300

    All work on phone without wifi (Work)

--- app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/MainActivity.java
+++ app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/MainActivity.java
@@ -2,29 +2,34 @@ package com.piteravto.rockabilla.tvvideoplayerbeta;
 
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.AsyncTask;
+
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.piteravto.rockabilla.tvvideoplayerbeta.api.ServerApi;
+import com.piteravto.rockabilla.tvvideoplayerbeta.controllers.WifiController;
 
-
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -34,19 +39,68 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private String command;
-    VideoView videoView;
+    private VideoView videoView;
+    private String videoToPlay;
+    private ArrayList<String> filesNames;
+    private ArrayList<String> downloadFilesName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //горизонтальная ориентация
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        try {
+            videoView = (VideoView) findViewById(R.id.videoview);
+            videoView.setMediaController(null);
+            videoView.requestFocus();
+            videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    //Start a new activity or do whatever else you want here
+                    //action(videoView);
+                    getCommand();
+                }
+            });
+            getFilesNames();
+
+            //WifiController.configApState(MainActivity.this);
+
+            getCommand();
+
+        } catch (Exception e) {
+            getCommand();
+            Toast.makeText(MainActivity.this, "in main Error", Toast.LENGTH_LONG).show();
+        }
+
+        //boolean con = WifiHostSpot.configApState(MainActivity.this);
+        //Toast.makeText(MainActivity.this, "Connection - " + con, Toast.LENGTH_SHORT).show();
+
 
     }
 
-    private void playVideo (String Path)
+
+    private void getFilesNames()
+    {
+        try {
+            String path = Environment.getExternalStorageDirectory().toString()+"/Download";
+            filesNames = new ArrayList<String>();
+            downloadFilesName = new ArrayList<String>();
+            File directory = new File(path);
+            File[] files = directory.listFiles();
+
+            for (int i = 0; i < files.length; i++)
+            {
+                filesNames.add(files[i].getName());
+            }
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "getFilesNames Error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    private void playVideo (String Path, VideoView videoView)
     {
         // установите свой путь к файлу на SD-карточке
         //String videoSource = "/storage/6605-E526/test.mp4";
@@ -54,31 +108,65 @@ public class MainActivity extends AppCompatActivity {
         //String videoSource = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/test.mp4";
         //Toast.makeText(MainActivity.this, videoSource, Toast.LENGTH_SHORT).show();
 
+        try {
+            if (Path == null || Path.length()>0) {
+                String[] tmp = Path.split("/");
 
-        videoView = (VideoView) findViewById(R.id.videoview);
-        videoView.setVideoPath(Path);
-        //убрали play и прочее
-        videoView.setMediaController(null);
-        videoView.requestFocus();
-        videoView.start(); // начинаем воспроизведение автоматически
+                String fileName = tmp[tmp.length - 1];
+                Toast.makeText(MainActivity.this, "file name to play " + fileName, Toast.LENGTH_LONG).show();
+                if (filesNames.contains(fileName)) {
+                    videoView.setVideoPath(Path);
+                    Toast.makeText(MainActivity.this, "start to play " + fileName, Toast.LENGTH_LONG).show();
+                    videoView.start(); // начинаем воспроизведение автоматически
+                } else {
+                    getCommand();
+                }
+            }
+            else
+            {
+                getCommand();
+            }
+        } catch (Exception e) {
+
+            Toast.makeText(MainActivity.this, "playVideo Error", Toast.LENGTH_LONG).show();
+            getCommand();
+        }
     }
 
-    private void downloadVideo(String url, String fileName, String description, String title)
+    private void downloadVideo(String url, final String fileName, String description, String title)
     {
         //String url = getString(R.string.server_name) + "tv-service/uploads/d9e28907f1a7d949c7d973cdcc2e363c.mp4";
+        Toast.makeText(MainActivity.this, "1 " + fileName, Toast.LENGTH_LONG).show();
+        if(filesNames.contains(fileName) || downloadFilesName.contains(fileName))
+                return;
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        Toast.makeText(MainActivity.this, "2 " + fileName, Toast.LENGTH_LONG).show();
         request.setDescription(description);
         request.setTitle(title);
-// in order for this if to run, you must use the android 3.2 to compile your app
+        // in order for this if to run, you must use the android 3.2 to compile your app
+        Toast.makeText(MainActivity.this, "3 " + fileName, Toast.LENGTH_LONG).show();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             request.allowScanningByMediaScanner();
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         }
+        Toast.makeText(MainActivity.this, "4 " + fileName, Toast.LENGTH_LONG).show();
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-
-// get download service and enqueue file
+        Toast.makeText(MainActivity.this, "5 " + fileName, Toast.LENGTH_LONG).show();
+        // get download service and enqueue file
         DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        Toast.makeText(MainActivity.this, "6 " + fileName, Toast.LENGTH_LONG).show();
         manager.enqueue(request);
+        Toast.makeText(MainActivity.this, "start download " + fileName, Toast.LENGTH_LONG).show();
+        BroadcastReceiver onComplete=new BroadcastReceiver() {
+            public void onReceive(Context ctxt, Intent intent) {
+                Toast.makeText(MainActivity.this, "finish download " + fileName, Toast.LENGTH_LONG).show();
+                filesNames.add(fileName);
+                downloadFilesName.remove(fileName);
+            }
+        };
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        Toast.makeText(MainActivity.this, "in download " + fileName, Toast.LENGTH_LONG).show();
+        downloadFilesName.add(fileName);
     }
 
 
@@ -90,40 +178,39 @@ public class MainActivity extends AppCompatActivity {
         ServerApi.getApi().getData(getString(R.string.tv_directory), getString(R.string.get_command)).enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                Toast.makeText(MainActivity.this, "Успешно подключился", Toast.LENGTH_LONG).show();
-
                 try {
                     command = response.body().string();
-                } catch (IOException e) {
-                    Toast.makeText(MainActivity.this, "Ошибка string()", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, command, Toast.LENGTH_SHORT).show();
+                    //смотрим какая это команда
+                    String[] splitCommand = command.split(" ");
+                    Toast.makeText(MainActivity.this, splitCommand[0], Toast.LENGTH_SHORT).show();
+                    if (splitCommand[0].equals("UPDATE"))
+                    {
+                        //вытаскиваем имя файла
+                        String[] tmp = splitCommand[1].split("/");
+                        String fileName = tmp[tmp.length-1];
+                        Toast.makeText(MainActivity.this, splitCommand[1], Toast.LENGTH_SHORT).show();
+                        downloadVideo(splitCommand[1], fileName, "Download from server " + getString(R.string.server_name), fileName);
+                    }
+                    else
+                    {
+                        videoToPlay = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + command;
+                    }
+
+                    playVideo(videoToPlay, videoView);
+
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "getCommand Error " + command, Toast.LENGTH_LONG).show();
+
+                    playVideo(videoToPlay, videoView);
                 }
-
-                //recyclerView.getAdapter().notifyDataSetChanged();
-
-                Toast.makeText(MainActivity.this, command, Toast.LENGTH_SHORT).show();
             }
-
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Ошибка подключения", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    //просто стучимся, чтобы дать знать, что мы еще живы
-    private void sendNotification()
-    {
-        ServerApi.getApi().getData(getString(R.string.tv_directory), getString(R.string.send_notification)).enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
 
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Ошибка подключения", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 
 }
---
+++ app/src/main/java/com/piteravto/rockabilla/tvvideoplayerbeta/controllers/WifiController.java
@@ -0,0 +1,42 @@
+package com.piteravto.rockabilla.tvvideoplayerbeta.controllers;
+
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by MishustinAI on 09.12.2016.
+ */
+
+public class WifiController {
+    public static boolean isApOn(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        try {
+            Method method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
+            method.setAccessible(true);
+            return (Boolean) method.invoke(wifimanager);
+        }
+        catch (Throwable ignored) {}
+        return false;
+    }
+
+    public static boolean configApState(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        WifiConfiguration wificonfiguration = null;
+        try {
+            // if WiFi is on, turn it off
+            if(isApOn(context)) {
+                wifimanager.setWifiEnabled(false);
+            }
+            Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(wifimanager, wificonfiguration, !isApOn(context));
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
